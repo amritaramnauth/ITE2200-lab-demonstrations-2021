e1bb9835100516b268ff785bdfc18fd2e472244a
Amrita Ramnauth <amritaramnauth@outlook.com>
2021-06-11 14:40:44 -0400

    postgres init file update

--- 04-sql2o-decomposition/chatapp/init.sql
+++ 04-sql2o-decomposition/chatapp/init.sql
@@ -1,11 +1,49 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users (
-    id serial PRIMARY KEY,
-    email VARCHAR(60) NOT NULL,
-    contact_number VARCHAR(15) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    age INT,
-    gender VARCHAR(7),
-    hobbies text,
-    characteristics text
+	email varchar(50) NOT NULL,
+	contact_number varchar(15) NOT NULL,
+	address varchar(50) NOT NULL,
+	username varchar(50) NOT NULL,
+	"characteristics" text NULL,
+	age int2 NULL,
+	gender varchar(7) NULL,
+	hobbies text NULL,
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	user_type varchar NOT NULL,
+	CONSTRAINT users_pk PRIMARY KEY (id)
 );
+
+CREATE TABLE rooms (
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	"name" varchar(50) NOT NULL,
+	CONSTRAINT rooms_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	sender_id uuid NOT NULL,
+	room_id uuid NOT NULL,
+	"content" text NOT NULL,
+	"type" varchar NOT NULL,
+	created_at varchar NOT NULL,
+	CONSTRAINT messages_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE room_users (
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	room_id uuid NOT NULL,
+	user_id uuid NOT NULL,
+	CONSTRAINT room_users_pk PRIMARY KEY (id)
+);
+
+-- messages foreign keys
+ALTER TABLE messages ADD CONSTRAINT messages_fk FOREIGN KEY (sender_id) REFERENCES users(id);
+ALTER TABLE messages ADD CONSTRAINT messages_fk_1 FOREIGN KEY (room_id) REFERENCES rooms(id);
+
+-- room_users foreign keys
+
+ALTER TABLE room_users ADD CONSTRAINT room_users_fk FOREIGN KEY (room_id) REFERENCES rooms(id);
+ALTER TABLE room_users ADD CONSTRAINT room_users_fk_1 FOREIGN KEY (user_id) REFERENCES users(id);
+
+
